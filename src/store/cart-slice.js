3d2d753f052436cbaaeab1d7d1d39e85efc4b92a
RubenSugianto <ruben.sugianto@gmail.com>
2025-03-30 23:43:07 +0700

    Using useEffect with Redux

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -29,7 +29,6 @@ const cartSlice = createSlice({
             const id = action.payload;
             const existingItem = state.items.find((item) => item.id === id);
             state.totalQuantity--;
-            state.changed = true;
             if(existingItem.quantity === 1) {
                 state.items = state.items.filter((item) => item.id !== id);
             } else {
